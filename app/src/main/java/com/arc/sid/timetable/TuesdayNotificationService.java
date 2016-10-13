923344c8ef4b37be86b80512cc4704a2aa2e15b6
SiddharthSaxena <siddharth.saxena25@gmail.com>
2016-10-13 19:43:00 +0530

    Update Tuesday notification service

---
+++ app/src/main/java/com/arc/sid/timetable/TuesdayNotificationService.java
@@ -0,0 +1,140 @@
+package com.arc.sid.timetable;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.NotificationCompat;
+
+public class TuesdayNotificationService extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setSmallIcon(R.drawable.icon);
+        builder.setContentTitle(context.getString(R.string.app_name));
+
+        if (intent.getAction().equalsIgnoreCase("FirstPeriod")) {
+            builder.setContentText("First Lecture: Wireless Communication begins in 5 minutes");
+            builder.setContentIntent(PendingIntent.getActivity(context, 200, new Intent(context, Tuesday.class), 0));
+            builder.setAutoCancel(true);
+            int notificationID = 200;
+            Notification notification = builder.build();
+            notificationManager.notify(notificationID, notification);
+
+            Handler handler = new Handler();
+            long delayInMilliseconds = 300000;
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    notificationManager.cancel(200);
+                }
+            }, delayInMilliseconds);
+        }
+
+        if (intent.getAction().equalsIgnoreCase("SecondPeriod")) {
+            builder.setContentText("Next Lecture: RF Devices and Circuits");
+            builder.setContentIntent(PendingIntent.getActivity(context, 201, new Intent(context, Tuesday.class), 0));
+            int notificationID = 201;
+            builder.setAutoCancel(true);
+            Notification notification = builder.build();
+            notificationManager.notify(notificationID, notification);
+
+            Handler handler = new Handler();
+            long delayInMilliseconds = 300000;
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    notificationManager.cancel(201);
+                }
+            }, delayInMilliseconds);
+        }
+
+        if (intent.getAction().equalsIgnoreCase("ThirdPeriod")) {
+            builder.setContentText("Next Lecture: Embedded Systems");
+            builder.setContentIntent(PendingIntent.getActivity(context, 202, new Intent(context, Tuesday.class), 0));
+            int notificationID = 202;
+            builder.setAutoCancel(true);
+            Notification notification = builder.build();
+            notificationManager.notify(notificationID, notification);
+
+            Handler handler = new Handler();
+            long delayInMilliseconds = 300000;
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    notificationManager.cancel(202);
+                }
+            }, delayInMilliseconds);
+        }
+
+        if (intent.getAction().equalsIgnoreCase("FourthPeriod")) {
+            builder.setContentText("Lunch Break coming up in 5 minutes");
+            builder.setContentIntent(PendingIntent.getActivity(context, 203, new Intent(context, Tuesday.class), 0));
+            int notificationID = 203;
+            builder.setAutoCancel(true);
+            Notification notification = builder.build();
+            notificationManager.notify(notificationID, notification);
+
+            Handler handler = new Handler();
+            long delayInMilliseconds = 300000;
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    notificationManager.cancel(203);
+                }
+            }, delayInMilliseconds);
+        }
+
+        if (intent.getAction().equalsIgnoreCase("FifthPeriod")) {
+            builder.setContentText("Next Lecture: Radar and Navigation");
+            builder.setContentIntent(PendingIntent.getActivity(context, 204, new Intent(context, Tuesday.class), 0));
+            int notificationID = 204;
+            builder.setAutoCancel(true);
+            Notification notification = builder.build();
+            notificationManager.notify(notificationID, notification);
+
+            Handler handler = new Handler();
+            long delayInMilliseconds = 300000;
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    notificationManager.cancel(204);
+                }
+            }, delayInMilliseconds);
+        }
+
+        if (intent.getAction().equalsIgnoreCase("SixthPeriod")) {
+            builder.setContentText("Next Lecture: Optical Communication");
+            builder.setContentIntent(PendingIntent.getActivity(context, 205, new Intent(context, Tuesday.class), 0));
+            int notificationID = 205;
+            builder.setAutoCancel(true);
+            Notification notification = builder.build();
+            notificationManager.notify(notificationID, notification);
+
+            Handler handler = new Handler();
+            long delayInMilliseconds = 300000;
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    notificationManager.cancel(205);
+                }
+            }, delayInMilliseconds);
+        }
+
+        if (intent.getAction().equalsIgnoreCase("SeventhPeriod")) {
+            builder.setContentText("Last Lecture: Optical Communication");
+            builder.setContentIntent(PendingIntent.getActivity(context, 206, new Intent(context, Tuesday.class), 0));
+            int notificationID = 206;
+            builder.setAutoCancel(true);
+            Notification notification = builder.build();
+            notificationManager.notify(notificationID, notification);
+
+            Handler handler = new Handler();
+            long delayInMilliseconds = 300000;
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    notificationManager.cancel(206);
+                }
+            }, delayInMilliseconds);
+        }
+    }
+}
